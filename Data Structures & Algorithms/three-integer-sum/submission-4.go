d04cd0c61da0f84acccb407db6fa4cd41443f410
Gideon <124469965+gideonadjei94@users.noreply.github.com>
2026-07-17 19:06:33 +0000

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.go
@@ -0,0 +1,35 @@
+func threeSum(nums []int) [][]int {
+    	sort.Ints(nums)
+	res := [][]int{}
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum > 0 {
+				right--
+			} else if sum < 0 {
+				left++
+			} else {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+                for left < right && nums[right] == nums[right + 1] {
+                    right--
+                }
+			}
+		}
+	}
+
+	return res
+}
